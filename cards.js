a6426f19de836062dcc093419c82567268858b01
SqueakyFlamingo <34474194+squeakyflamingo@users.noreply.github.com>
2023-01-06 15:49:51 +0100

    Basic setup.

---
+++ cards.js
@@ -0,0 +1,71 @@
+let xCard = {
+    text: "Bei schweren Extremitäten-Blutungen: Tourniquet!",
+    addText: "",
+    response: "single",
+    color: "darkred",
+    tColor: "white"
+}
+
+let greenCard = {
+    text: "Sammelstelle definieren, gehfähige Patienten dort hinschicken!",
+    addText: "",
+    response: "single",
+    color: "darkgreen",
+    tColor: "white"
+}
+
+let a1Card = {
+    text: "Offenhalten der Atemwege erforderlich?",
+    addText: "mit Guedel-/Wendl-Tubus und Seitenlage",
+    response: "dual",
+    color: "red",
+    tColor: "white"
+}
+
+let a2Card = {
+    text: "Inhalationstrauma mit Stridor?",
+    addText: "",
+    response: "dual",
+    color: "red",
+    tColor: "white"
+}
+
+let bCard = {
+    text: "Atemfrequenz <10/min oder >30/min?",
+    addText: "Untersuchungsdauer 10 Sek.",
+    response: "dual",
+    color: "red",
+    tColor: "white"
+}
+
+let c1Card = {
+    text: "Unstillbare Blutung?",
+    addText: "",
+    response: "dual",
+    color: "red",
+    tColor: "white"
+}
+
+let c2Card = {
+    text: "Kein Radialispuls?",
+    addText: "Untersuchungsdauer 10 Sek.",
+    response: "dual",
+    color: "red",
+    tColor: "white"
+}
+
+let dCard = {
+    text: "Kann Kommando \"Heben Sie den Arm\" <strong>nicht<\\strong> ausführen?",
+    addText: "Untersuchungsdauer 10 Sek.",
+    response: "dual",
+    color: "red",
+    tColor: "white"
+}
+
+let endCard = {
+    text: "Keiner der genannten Punkte zutreffend?",
+    addText: "",
+    response: "dual",
+    color: "yellow",
+    tColor: "black"
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+let cardContainer = document.getElementById("card")
+
+let cardArray = [
+    xCard, greenCard, a1Card, a2Card, bCard, c1Card, c2Card, dCard, endCard
+]
