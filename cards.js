15857d494c8be24b85e9424baf6b09646f970dd6
SqueakyFlamingo <34474194+squeakyflamingo@users.noreply.github.com>
2024-04-15 15:12:59 +0200

    Completed build.

--- cards.js
+++ cards.js
@@ -18,7 +18,8 @@ let a1Card = {
     text: "Offenhalten der Atemwege erforderlich?",
     addText: "mit Guedel-/Wendl-Tubus und Seitenlage",
     response: "dual",
-    color: "red",
+    cat: "red",
+    color: "darkred",
     tColor: "white"
 }
 
@@ -26,7 +27,8 @@ let a2Card = {
     text: "Inhalationstrauma mit Stridor?",
     addText: "",
     response: "dual",
-    color: "red",
+    cat: "red",
+    color: "darkred",
     tColor: "white"
 }
 
@@ -34,7 +36,8 @@ let bCard = {
     text: "Atemfrequenz <10/min oder >30/min?",
     addText: "Untersuchungsdauer 10 Sek.",
     response: "dual",
-    color: "red",
+    cat: "red",
+    color: "darkred",
     tColor: "white"
 }
 
@@ -42,7 +45,8 @@ let c1Card = {
     text: "Unstillbare Blutung?",
     addText: "",
     response: "dual",
-    color: "red",
+    cat: "red",
+    color: "darkred",
     tColor: "white"
 }
 
@@ -50,22 +54,48 @@ let c2Card = {
     text: "Kein Radialispuls?",
     addText: "Untersuchungsdauer 10 Sek.",
     response: "dual",
-    color: "red",
+    cat: "red",
+    color: "darkred",
     tColor: "white"
 }
 
 let dCard = {
-    text: "Kann Kommando \"Heben Sie den Arm\" <strong>nicht<\\strong> ausführen?",
+    text: "Kann Kommando \"Heben Sie den Arm\" <strong>nicht</strong> ausführen?",
     addText: "Untersuchungsdauer 10 Sek.",
     response: "dual",
-    color: "red",
+    cat: "red",
+    color: "darkred",
     tColor: "white"
 }
 
 let endCard = {
     text: "Keiner der genannten Punkte zutreffend?",
     addText: "",
-    response: "dual",
-    color: "yellow",
+    response: "single",
+    color: "gold",
     tColor: "black"
+}
+
+let redCard = {
+    text: "Einstufung in Kategorie 1 (rot)",
+    addText: "akute, vitale Bedrohung, Sofortbehandlung",
+    response: "restart",
+    color: "darkred",
+    tColor: "white"
+}
+
+let yellowCard = {
+    text: "Einstufung in Kategorie 2 (rot)",
+    addText: "schwere Verletzung, aufgeschobene, dringliche Behandlung",
+    response: "restart",
+    color: "gold",
+    tColor: "black"
+}
+
+let infoCard = {
+    text: "Info<br>mStartled-Mini-Pseudo-App",
+    addText: "Erstellt von Tim, 2024",
+    response: "restart",
+    color: "grey",
+    tColor: "white"
 }
--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-let cardContainer = document.getElementById("card")
-
-let cardArray = [
+let cards = [
     xCard, greenCard, a1Card, a2Card, bCard, c1Card, c2Card, dCard, endCard
 ]
+
+let index = 0;
+
+let loadCard = (c) => {
+    text.innerHTML = c.text
+    addText.innerHTML = c.addText
+    card.style.backgroundColor = c.color
+    card.style.color = c.tColor
+    
+    dual.classList.toggle("d-none", c.response !== "dual")
+    single.classList.toggle("d-none", c.response !== "single")
+    restart.classList.toggle("d-none", c.response !== "restart")
+}
+
+let resetAction = () => {
+    window.location.reload()
+}
+
+let infoAction = () => {
+    loadCard(infoCard)
+}
+
+let yesAction = () => {
+    loadCard(redCard)
+}
+
+let noAction = continueAction = () => {
+    if(index === cards.length-1) {
+        loadCard(yellowCard)
+        return
+    } 
+    
+    loadCard(cards[index++])
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadCard(cards[index])
+})
